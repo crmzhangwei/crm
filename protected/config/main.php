5330f022b071b85167e86f9046d211208420b48c
crmzhangwei <ndzhangwei16@163.com>
2015-03-01 00:52:16 +0800

    更改默认控制器

--- protected/config/main.php
+++ protected/config/main.php
@@ -16,7 +16,7 @@ return array(
 		'application.models.*',
 		'application.components.*',
 	),
-
+        'defaultController'=>'default',
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
 		'User',
