634122ebb509c03a1b6c43c56e1cbaa431077107
lanzz2015 <jacks_S@126.com>
2015-04-12 23:43:04 +0800

    获取部门组别人员信息

--- protected/components/Userinfo.php
+++ protected/components/Userinfo.php
@@ -5,12 +5,12 @@ class Userinfo
 	/**
 	*获取公共组别信息
 	*/
-	public static function getGroup()
+	/*public static function getGroup()
 	{
 		$modelgroup = GroupInfo::model();
 		$groupArr =  CHtml::listData(GroupInfo::model()->findAll(), 'id', 'name');
 		return $groupArr;
-	}
+	}*/
 
 	public static function getUserbygid($gid)
 	{
@@ -24,6 +24,23 @@ class Userinfo
 	    	$Users[$value['eno']] = $value['username'];
 	    }
         return $Users;
+	}
+
+	public static function getGroupById($deptid){
+		
+		$allData = Yii::app()->db->createCommand("select g.`group_id`,i.`name` from `c_dept_group` as g left join `c_group_info` as i 
+			on g.`group_id`=i.`id` where dept_id = :deptid")->queryAll(TRUE,array(":deptid"=>$deptid));
+		$groupArr = array();
+		foreach ($allData as $k => $v) {
+			$groupArr[$v['group_id']] = $v['name'];
+		}
+		return $groupArr;
+	}
+
+	public static function getDept(){
+		$modelDept = DeptInfo::model();
+		$deptArr = CHtml::listData($modelDept->findAll(),'id', 'name');
+		return $deptArr;
 	}
 
 }
