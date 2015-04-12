adffe7362bdbf66a25e587f8a2a044adf3b7e6b6
lanzz2015 <jacks_S@126.com>
2015-04-12 23:38:41 +0800

    添加部门模型

--- protected/config/main.php
+++ protected/config/main.php
@@ -24,7 +24,7 @@ return array(
 		// uncomment the following to enable the Gii tool
 		'User',
 		'Customer',
-		'DeptInfo',
+		'Deptinfo',
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>false,
