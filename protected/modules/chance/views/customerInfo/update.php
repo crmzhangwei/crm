a5f9672500aaaec02122a645e6a13c862e50b88b
Aoseala <aoseala@gmail.com>
2015-04-12 22:53:33 +0800

    完善功能

--- protected/modules/chance/views/customerInfo/update.php
+++ protected/modules/chance/views/customerInfo/update.php
@@ -3,19 +3,9 @@
 /* @var $model CustomerInfo */
 
 $this->breadcrumbs=array(
-	'Customer Infos'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
-	'Update',
+	'安排联系机会'=>array('admin'),
+	'客户详情',
 );
 
-$this->menu=array(
-	array('label'=>'List CustomerInfo', 'url'=>array('index')),
-	array('label'=>'Create CustomerInfo', 'url'=>array('create')),
-	array('label'=>'View CustomerInfo', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage CustomerInfo', 'url'=>array('admin')),
-);
 ?>
-
-<h1>Update CustomerInfo <?php echo $model->id; ?></h1>
-
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
