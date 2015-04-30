991f4cb46ca94bf241b9157904695f13fc527c93
crmzhangwei <ndzhangwei16@163.com>
2015-05-01 00:27:49 +0800

    用户组别管理

--- protected/modules/User/controllers/DeptInfoController.php
+++ protected/modules/User/controllers/DeptInfoController.php
@@ -8,42 +8,6 @@ class DeptInfoController extends GController
 	 */
 	//public $layout='//layouts/column2';
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
-
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
 
 	/**
 	 * Displays a particular model.
@@ -71,10 +35,12 @@ class DeptInfoController extends GController
 		{
 			$model->attributes=$_POST['DeptInfo'];
 			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
+			 Utils::showMsg (1, '组名增加成功!');
+                         else
+                           Utils::showMsg (0, '组名增加失败!');
 		}
 
-		$this->render('create',array(
+		$this->renderPartial('update',array(
 			'model'=>$model,
 		));
 	}
@@ -84,8 +50,10 @@ class DeptInfoController extends GController
 	 * If update is successful, the browser will be redirected to the 'view' page.
 	 * @param integer $id the ID of the model to be updated
 	 */
-	public function actionUpdate($id)
+	public function actionUpdate($id=0)
 	{
+           
+                $id = $id ? $id: $_POST['DeptInfo']['id'];
 		$model=$this->loadModel($id);
 
 		// Uncomment the following line if AJAX validation is needed
@@ -95,10 +63,13 @@ class DeptInfoController extends GController
 		{
 			$model->attributes=$_POST['DeptInfo'];
 			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
+			   Utils::showMsg (1, '组名修改成功!');
+                         else
+                           Utils::showMsg (0, '组名修改失败!');
+                         exit;
 		}
 
-		$this->render('update',array(
+		$this->renderPartial('update',array(
 			'model'=>$model,
 		));
 	}
