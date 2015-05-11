50c973343eac81912bc7e8386b0bf21725628d71
crmzhangwei <ndzhangwei16@163.com>
2015-05-11 23:22:43 +0800

    部门组别关系设置管理

--- protected/modules/User/views/deptGroup/admin.php
+++ protected/modules/User/views/deptGroup/admin.php
@@ -1,55 +1,182 @@
 <?php
-/* @var $this DeptGroupController */
-/* @var $model DeptGroup */
 
-$this->breadcrumbs=array(
-	'Dept Groups'=>array('index'),
-	'Manage',
+$this->pageTitle = '部门-组别管理页面';
+$this->breadcrumbs = array(
+    '部门组别管理' => array('/User/deptgroup/admin'),
+    '部门-组别管理页面',
 );
-
-$this->menu=array(
-	array('label'=>'List DeptGroup', 'url'=>array('index')),
-	array('label'=>'Create DeptGroup', 'url'=>array('create')),
-);
-
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#dept-group-grid').yiiGridView('update', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
 ?>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="tabbable">
+            <ul class="nav nav-tabs padding-12 tab-color-blue" id="myTab4">
+                
+                <li class="active">
+                    <a href="<?php echo Yii::app()->createUrl('/User/deptgroup/admin'); ?>">部门组别分配</a>
+                </li>
+               
+            </ul>
 
-<h1>Manage Dept Groups</h1>
+            <div class="tab-content">
+                <div id="dropdown14" class="tab-pane in active">    
+                    <div class="row">
+                        <div class="row">
+                            <div class="col-xs-7" style="padding-left:50px;">
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
+                            </div> 
+                            <div class="col-xs-5" style="padding-right:50px;">
+                                <!-- .右侧保存按钮 -->
+                                <div style="float: right;">
+                                    <button type="button" id="saveBtn" class="btn btn-primary btn-sm">  
+                                        <i class="icon-save"></i>
+                                        保存设置
+                                    </button> 
+                                </div>
+                            </div> 
+                        </div>
+                        <div class="row">
+                            <!-- .header -->
+                            <div class="col-sm-12 no-padding-top">
+                                <div class="col-xs-7" style="padding-left:50px;">
+                                    <!-- .表头说明部分 -->
+                                    <h5 class="row header lighter">
+                                        <span class="col-xs-6 smaller"> 选择部门：<strong id="selRoleName"></strong></span>
+                                        <input type="hidden" name="roleid" id="roleId" value="">
+                                    </h5>
+                                </div>
+                                <div class="col-xs-5" style="padding:0 50px 0 50px;">
+                                    <!-- .树说明 -->
+                                    <div class="col-sm-12 no-padding-top header lighter">
+                                        选择相应组别：
+                                    </div>  <!-- .header -->
+                                </div>  <!-- .header -->
+                            </div>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+                            <div class="col-xs-7" style="padding-left:50px; border-right: 1px dashed #ccc;">
+                                <!--表格部分-->
+                                <div class="col-sm-12 no-padding-top">
+                                    <div class="row">
+                                        <table id="roles_list" class="table table-bordered table-projects table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th class="text-center" style="width: 45px;">ID</th>
+                                                    <th class="text-center">部门名称</th>
+                                                  
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php 
+                                                 
+                                                 $dataProvider=new CActiveDataProvider('DeptInfo');
+                                                 $dataProvider->pagination->pageVar = 'page';
+                                                 $dept_list = $dataProvider->getData();
+                                                 if($dept_list):
+                                                    foreach($dept_list as $k=>$v):
+                                                ?>
+                                                    <tr roleid="<?php echo $v['id'];?>">
+                                                        <td class="text-right"><?php echo $v['id'];?></td>
+                                                        <td class="text-left"><?php echo $v['name'];?></td>
+                                                      
+                                                    </tr>
+                                                <?php
+                                                    endforeach;
+                                                endif;
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div><!-- end table-list -->
+                                <!-- .pagination分页 -->
+                                <div class="row table-page">
+                                    <div class="col-sm-6" style="padding-left: 20px;"> 
+                                        共<span class="orange"><?= $dataProvider->totalItemCount ?></span>条记录
+                                    </div>
+                                    <div class="col-sm-6 no-padding-right"> 
+                                        <?php
+                                        $this->widget('GLinkPager', array(
+                                                'pages' => $dataProvider->pagination,
+                                            )
+                                        );
+                                        ?>
+                                    </div>
+                                </div> <!-- .pagination -->
+                            </div>
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'dept-group-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'id',
-		'dept_id',
-		'group_id',
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
-)); ?>
+                             <div class="col-xs-5" style="padding-left:50px;">
+                                <!-- .树部分 -->
+                                <div class="col-sm-12 no-padding-top">
+                                    <div class="row">
+                                        <div class="row">
+                                            <div class="widget-main padding-8">
+                                                <div id="permission_tree"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/js/jstree/themes/default/style.min.css" type="text/css">
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/assets/js/jstree/jstree.min.js"></script>
+<script type="text/javascript">
+    jQuery(function($) {
+        $('#permission_tree').jstree({'plugins': ["checkbox"], 
+            "checkbox" : {
+                'tie_selection': true,
+               },
+            'core': {
+            'data': <?php echo $permission; ?>
+        }});
+        var tree = $('#permission_tree');
+         $("#roles_list tbody tr").css({"cursor":"pointer"}).bind("click", function(){
+            var roleid = $(this).attr( 'roleid' );
+            $("#roleId").val( roleid );
+            $("#roles_list tbody tr").each(function(i, e){
+                $(this).css({"background":"none" }).removeClass("red");
+            });
+            $(this).css({"background-color":"#E5E5E5"}).addClass("red");
+            $("#selRoleName").html( $(this).find("td:eq(1)").text() );
+            tree.jstree(true).close_all();
+            tree.jstree(true).uncheck_all();
+            $.ajax({
+                url: '<?php echo $this->createUrl('/User/deptgroup/selectRolePermission');?>',
+                type: 'post',
+                data: {roleid: roleid},
+                dataType: 'json',
+            
+                success: function(result){
+                    
+                    $(result).each(function(i, data){
+                        $('#permission_tree').jstree(true).select_node( data.id );
+                    });
+                },
+        
+            });
+            
+        });
+        
+        $("#saveBtn").click(function(){
+            var _this = $(this);
+            var nodes = $("#permission_tree").jstree("get_checked");
+            var roleid = $("#roleId").val();
+            if(roleid == '') {
+                bootbox.alert('请选择部门');
+                return;
+            }                
+           public.Ajax({
+                url: '<?php echo $this->createUrl('/User/deptgroup/assignRolePermission');?>',
+                data: {roleid: roleid, pids: nodes},
+                callback: function(result){
+                    bootbox.alert( result.msg );
+                }
+            });
+            
+        });
+        
+    });
+</script>
