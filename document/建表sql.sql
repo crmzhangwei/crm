c6cf58a042306f71d5f77250fb0589bb10af37e2
crmzhangwei <ndzhangwei16@163.com>
2015-05-16 21:00:33 +0800

    修改字段的默认值

--- document/建表sql.sql
+++ document/建表sql.sql
@@ -1,24 +1,25 @@
 ﻿CREATE TABLE `c_users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `eno` varchar(10) NOT NULL DEFAULT '' COMMENT '工号',
-  `pass` varchar(50) NOT NULL COMMENT '密码',
+  `pass` varchar(50) NOT NULL DEFAULT '' COMMENT '密码',
   `name` varchar(20) DEFAULT '' COMMENT '姓名',
   `username` varchar(30) NOT NULL COMMENT '用户名',
   `birth` date DEFAULT NULL COMMENT '生日',
   `sex` tinyint(2) NOT NULL DEFAULT '1' COMMENT '性别',
-  `tel` varchar(20) NOT NULL COMMENT '电话号码',
-  `qq` varchar(15) DEFAULT NULL COMMENT 'qq',
-  `dept_id` mediumint(4) NOT NULL COMMENT '部门',
-  `group_id` mediumint(5) DEFAULT NULL COMMENT '组别',
-  `manager_id` int not null COMMENT '主管id',
-  `ismaster` tinyint(1) DEFAULT NULL COMMENT '是否精英',
-  `status` tinyint(2) DEFAULT NULL COMMENT '状态',
+  `tel` varchar(20) NOT NULL DEFAULT '' COMMENT '电话号码',
+  `qq` varchar(15) DEFAULT 0 COMMENT 'qq',
+  `dept_id` mediumint(4) NOT NULL  DEFAULT 0 COMMENT '部门',
+  `group_id` mediumint(5) DEFAULT 0 COMMENT '组别',
+  `manager_id` int not nullDEFAULT 0  COMMENT '主管id',
+  `ismaster` tinyint(1) DEFAULT 2 COMMENT '是否精英',
+  `status` tinyint(2) DEFAULT 1 COMMENT '状态',
   `cust_num` int NOT NULL DEFAULT 0 COMMENT '已分配资源数',
   `extend_no` int NOT NULL DEFAULT 0 COMMENT '分机号',
-  `create_time` int NOT NULL COMMENT '创建时间',
-  `login_time` int NOT NULL COMMENT '最后登录时间',
+  `create_time` int NOT NULL DEFAULT 0 COMMENT '创建时间',
+  `login_time` int NOT NULL DEFAULT 0 COMMENT '最后登录时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
 
 
 DROP TABLE IF EXISTS `c_dept_group`;
