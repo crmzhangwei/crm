3e73ad09892277155fc5a4c0fd4156434795ca23
lanzz2015 <jacks_S@126.com>
2015-07-12 16:20:49 +0800

    数据报表

--- protected/modules/Statistics/controllers/CountController.php
+++ protected/modules/Statistics/controllers/CountController.php
@@ -2,7 +2,7 @@
 
 class CountController extends GController
 {
-    private $pageSize = 2;
+    private $pageSize = 10;
 	public function actionIndex()
 	{
             
@@ -58,7 +58,8 @@ class CountController extends GController
         }
 		
 	public function actionYeji(){
-		$page = max(Yii::app()->request->getParam('page'), 1);
+		//$page = max(Yii::app()->request->getParam('page'), 1);
+		//$offset = ($page-1)*$this->pageSize;
 		$search = Yii::app()->request->getParam("search");
 		if($search){
 			$where  = Utils::addWhere($search);
@@ -67,29 +68,7 @@ class CountController extends GController
 			$where = '';
 		}
 		
-		$offset = ($page-1)*$this->pageSize;
-		$param = array();
-
-		//这一部分获取总记录行
-		/*$sql = "select id,dial_time as d,dial_long as l from {{dial_detail}} where 1";
-		$criteria = new CDbCriteria();
-		$result1 = Yii::app()->db->createCommand($sql)->query();
-		$pages = new CPagination($result1->rowCount);
-
-		//获取查询的条数
-		$pages->pageSize = $this->pageSize;
-		$pages->applyLimit($criteria);
-		$result = Yii::app()->db->createCommand($sql . " LIMIT :offset,:limit");
-		$result->bindValue(':offset', $pages->currentPage * $pages->pageSize);
-		$result->bindValue(':limit', $pages->pageSize);
-		$res = $result->queryAll();*/
-		//部门 组别 二组联动
-		$deptArr = Userinfo::getDept();
-		$deptArr = array('0'=>'--请选择部门--') + $deptArr;
-		$groupArr = Userinfo::getGroupById(1);
-		$groupArr = array('0'=>'--请选择组别--') + $groupArr;
-		
-		$sql = "select d.name as dname,g.name as gname,acct_amount as amount,acct_number as number "
+		$sql = "select d.name as dname,g.name as gname, u.name as uname, acct_amount as amount,acct_number as number "
 				. "from {{finance}} as f "
 				. "left join {{users}} as u on f.sale_user=u.id "
 				. "left join {{dept_info}} as d on u.dept_id=d.id "
@@ -97,37 +76,147 @@ class CountController extends GController
 		$result = Yii::app()->db->createCommand($sql)->queryAll();
 		$total = 0;
 		$resArr = array();
-		if($result){
-			$deptGrp = array();
-			foreach ($result as $k=>$v){
-				$deptGrp[] = $v['dname'].$v['gname'];
-			}
-			$deptGrp = array_unique($deptGrp);
-			foreach ($deptGrp as $k1 => $v1) {
-				$resArr[$v1]['amount'] = 0;
-				$resArr[$v1]['number'] = 0;
-				foreach ($result as $k2 => $v2) {
-					$str = $v2['dname'].$v2['gname'];
-					if($str === $v1){
-						$resArr[$v1]['dname'] = $v2['dname'];
-						$resArr[$v1]['gname'] = $v2['gname'];
-						$resArr[$v1]['amount'] += $v2['amount'];
-						$resArr[$v1]['number'] += $v2['number'];
+		if($result){	
+			if($search['dept'] && $search['group'] && $search['users']){
+				$dgUser = array();
+				foreach ($result as $k=>$v){
+					$dgUser[] = $v['dname'].$v['gname'].$v['uname'];
+				}
+				$dgUser = array_unique($dgUser);
+				
+				foreach ($dgUser as $k1 => $v1) {
+					$resArr[$v1]['amount'] = 0;
+					$resArr[$v1]['number'] = 0;
+					foreach ($result as $k2 => $v2) {
+						$str = $v2['dname'].$v2['gname'].$v2['uname'];
+						if($str === $v1){
+							$resArr[$v1]['dname'] = $v2['dname'];
+							$resArr[$v1]['gname'] = $v2['gname'];
+							$resArr[$v1]['uname'] = $v2['uname'];
+							$resArr[$v1]['amount'] += $v2['amount'];
+							$resArr[$v1]['number'] += $v2['number'];
+						}
 					}
 				}
+				$total = count($resArr);
 			}
-			$total = count($resArr);
+			elseif($search['dept'] && $search['group']){
+				$dgroup = array();
+				foreach ($result as $k=>$v){
+					$dgroup[] = $v['dname'].$v['gname'];
+				}
+				$dgroup = array_unique($dgroup);
+				
+				foreach ($dgroup as $k1 => $v1) {
+					$resArr[$v1]['amount'] = 0;
+					$resArr[$v1]['number'] = 0;
+					foreach ($result as $k2 => $v2) {
+						$str = $v2['dname'].$v2['gname'];
+						if($str === $v1){
+							$resArr[$v1]['dname'] = $v2['dname'];
+							$resArr[$v1]['gname'] = $v2['gname'];
+							$resArr[$v1]['amount'] += $v2['amount'];
+							$resArr[$v1]['number'] += $v2['number'];
+						}
+					}
+				}
+				
+				$total = count($resArr);
+			}
+			else{
+				$depts = array();
+				foreach ($result as $k=>$v){
+					$depts[] = $v['dname'].$v['gname'];
+				}
+				$depts = array_unique($depts);
+				foreach ($depts as $k1 => $v1) {
+					$resArr[$v1]['amount'] = 0;
+					$resArr[$v1]['number'] = 0;
+					foreach ($result as $k2 => $v2) {
+						$str = $v2['dname'].$v2['gname'];
+						if($str === $v1){
+							$resArr[$v1]['dname'] = $v2['dname'];
+							$resArr[$v1]['gname'] = $v2['gname'];
+							$resArr[$v1]['amount'] += $v2['amount'];
+							$resArr[$v1]['number'] += $v2['number'];
+						}
+					}
+				}
+				$total = count($resArr);
+			}
 		}
+		
+		if($resArr){
+			$amount = array();
+			foreach ($resArr as $k=>$v){
+				$amount[] = $v['amount'];
+			}
+			array_multisort($amount, SORT_DESC, $resArr);
+		}
+		
+		//部门组别人员三级联动
+		$uInfo = Userinfo::secondlevel();
 		$data = array(
-			//'pages' => $pages,
 			'total' => $total,
-			//'crmlist' => $res,
 			'search' => $search,
-			'deptArr' => $deptArr,
-			'user' => $groupArr,
+			'deptArr' => $uInfo['deptArr'],
+			//'user' => $uInfo['groupArr'],
+			'infoArr'=>$uInfo['infoArr'],
+			'user_info'=>$uInfo['user_info'],
 			'resArr' => $resArr,
 		);
 		$this->render("yeji", $data);
+	}
+
+	public function actionMonth(){
+		$sql = "select FROM_UNIXTIME(acct_time,'%Y年%m月') as acct_time,SUM(acct_amount) as amount,SUM(acct_number) as number from `c_finance` 
+					where FROM_UNIXTIME(acct_time,'%Y')='2015' group by FROM_UNIXTIME(acct_time,'%Y%m')";
+		$result = Yii::app()->db->createCommand($sql)->queryAll();
+		$total = 0;
+		if($result){
+			$amount = array();
+			foreach ($result as $k => $v) {
+				$amount[] = $v['amount'];
+			}
+			array_multisort($amount, SORT_DESC, $result);
+			$total = count($result);
+		}
+		$ret = array('result'=>$result, 'total'=>$total);
+		$this->render("month", $ret);
+	}
+	
+	public function actionEveryday(){
+		$search = Yii::app()->request->getParam("search");
+		//$times = strtotime(date('Y-m', time()));
+		$stime = strtotime( $this->getCurMonthFirstDay(date('Y-m',time())) );//当月第一天,格式:2015-07-01
+		$ftime = strtotime( $this->getNextMonthFirstDay(date('Y-m',time())) );//2015-08-01
+		if(isset($search['stime'])){
+			$stime = strtotime( $this->getCurMonthFirstDay($search['stime']) );
+			$ftime = strtotime( $this->getNextMonthFirstDay($search['stime']) );
+		}
+		$sql = "select FROM_UNIXTIME(acct_time,'%Y年%m月%d') as acct_time, SUM(acct_number) as number,SUM(acct_amount) as amount "
+				. "from `c_finance` where acct_time>=$stime and acct_time<$ftime group by FROM_UNIXTIME(acct_time,'%Y%m%d') order by amount desc";
+		
+		$result = Yii::app()->db->createCommand($sql)->queryAll();
+		$total = 0;
+		if($result){
+			$total = count($result);
+		}
+		$ret = array('result'=>$result, 'total'=>$total, 'search'=>$search);
+		
+		$this->render("everyday",$ret);
+	}
+	
+	function getCurMonthFirstDay($date) {//获取本月第一天
+		return date('Y-m-01', strtotime($date));
+	}
+
+	function getCurMonthLastDay($date) {//获取本月最后一天
+		return date('Y-m-d', strtotime(date('Y-m-01', strtotime($date)) . ' +1 month -1 day'));
+	}
+	
+	function getNextMonthFirstDay($date) {//获取下月第一天
+		return date('Y-m-d', strtotime(date('Y-m-01', strtotime($date)) . ' +1 month'));
 	}
 
 }
